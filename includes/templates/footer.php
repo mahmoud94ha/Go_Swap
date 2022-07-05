466d73e4782ccec4b110012839c16bd26383d416
mahmoud94ha <mahmoudhannani3@gmail.com>
2022-07-05 21:43:04 +0100

    update

---
+++ includes/templates/footer.php
@@ -0,0 +1,28 @@
+  <!-- Site footer -->
+    <footer class="site-footer">
+<h4 style="text-align:center; ">
+Goswap
+</h4>
+        
+    </footer>
+
+    <script src="<?php echo $js ?>jquery-3.3.1.min.js" ></script>  
+    <script src="<?php echo $js ?>jquery-ui.min.js" ></script> 
+    <script src="<?php echo $js ?>front.js"></script>
+    <script src="<?php echo $js ?>bootstrap.min.js"></script>
+    <script src="<?php echo $js ?>login.js"></script>
+    <script src="<?php echo $js ?>store.js"></script>
+    <script src="<?php echo $js ?>show.js"></script>
+    <script src="<?php echo $js ?>main.js"></script>
+    <script src="<?php echo $js ?>ajout.js"></script>
+    <script src="<?php echo $js ?>tagsinput.js"></script>
+    <script src="<?php echo $js ?>search.js"></script>
+    <script src="<?php echo $js ?>messages.js"></script>
+    <script src="<?php echo $js ?>settings.js"></script>
+    <script src="layout/intl-tel-input-master/build/js/intlTelInput.js"></script>
+    <script>
+      var input = document.querySelector("#phone");
+      window.intlTelInput(input);
+    </script>
+    </body>
+</html>
